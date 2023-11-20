589a68b743cfd88a0c3b3b177be68cdd7857c360
DinhVanPhat <dinhvanphat9404@gmail.com>
2023-11-20 21:05:19 +0700

    update

---
+++ BrotherCafe/src/com/cafe/dao/SanPhamDAO.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cafe.dao;
+
+import com.cafe.model.SanPham;
+import com.cafe.utils.jdbcHelper;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SanPhamDAO {
+    String INSERT_SQL = "INSERT INTO SanPham (MaSP, TenSP, LoaiSP, Gia, HinhAnh, GioiThieu) VALUES (?, ?, ?, ?, ?, ?)";
+    String UPDATE_SQL = "UPDATE SanPham SET TenSP = ?, LoaiSP = ?, Gia = ?, HinhAnh = ?, GioiThieu = ? WHERE MaSP = ?";
+    String DELETE_SQL = "DELETE FROM SanPham WHERE MaSP = ?";
+    String SELECT_ALL_SQL = "SELECT * FROM SanPham";
+    String SELECT_BY_ID_SQL = "SELECT * FROM SanPham WHERE MaSP = ?";
+
+    public void insert(SanPham e) {
+        jdbcHelper.update(INSERT_SQL, e.getMaSP(), e.getTenSP(), e.getLoaiSP(), e.getGia(), e.getHinhAnh(), e.getGioiThieu());
+    }
+
+    public void update(SanPham e) {
+        jdbcHelper.update(UPDATE_SQL, e.getTenSP(), e.getLoaiSP(), e.getGia(), e.getHinhAnh(), e.getGioiThieu(), e.getMaSP());
+    }
+
+    public void delete(String id) {
+        jdbcHelper.update(DELETE_SQL, id);
+    }
+
+    public SanPham selectById(String id) {
+        List<SanPham> list = this.selectBySql(SELECT_BY_ID_SQL, id);
+        if (list.isEmpty()) {
+            return null;
+        }
+        for (SanPham nv : list) {
+            if (!nv.getMaSP().equals(id)) {
+                return null;
+            }
+        }
+        return list.get(0);
+    }
+
+    public List<SanPham> selectAll() {
+        return this.selectBySql(SELECT_ALL_SQL);
+    }
+
+    protected List<SanPham> selectBySql(String sql, Object... args) {
+        List<SanPham> list = new ArrayList<SanPham>();
+        try {
+            ResultSet rs = jdbcHelper.query(sql, args);
+            while (rs.next()) {
+                SanPham entity = new SanPham();
+                entity.setMaSP(rs.getString("MaSP"));
+                entity.setTenSP(rs.getString("TenSP"));
+                entity.setLoaiSP(rs.getString("LoaiSP"));
+                entity.setGia(rs.getString("Gia"));
+                entity.setHinhAnh(rs.getString("HinhAnh"));
+                entity.setGioiThieu(rs.getString("GioiThieu"));
+                list.add(entity);
+            }
+            rs.getStatement().getConnection().close();
+            return list;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<SanPham> selectByKeyWord(String keyword) {
+        String sql = "SELECT * FROM SanPham WHERE tenSP LIKE ? OR maSP LIKE ? OR loaiSP LIKE ? OR gia LIKE ?";
+        return this.selectBySql(sql, "%" + keyword + "%", "%" + keyword + "% ", "%" + keyword + "% ", "%" + keyword + "% ");
+    }
+
+    public boolean chechTrungMa(String ma) {
+        List<SanPham> list = this.selectAll();
+        for (SanPham nv : list) {
+            if (nv.getMaSP().equals(ma)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
